b6bd6ee609d068c58fe644efb8ed30f24faf375d
Marco Salsiccia <marco.salsiccia@gmail.com>
2018-08-12 19:11:11 -0700

    Added flavor text. Calculates rate and adds rate, hours worked, and amount owed to generated document. Added formatting.

--- invoicerator.py
+++ invoicerator.py
@@ -1,9 +1,20 @@
 from docx import Document
-
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 document = Document()
 
+document.add_heading('Marco Salsiccia')
+document.add_paragraph('901 Santiago St.')
+document.add_paragraph('San Francisco CA 94116')
+document.add_paragraph('408-314-5401')
+document.add_paragraph('marco.salsiccia@gmail.com')
+document.add_heading('Invoice', level=2).alignment=WD_ALIGN_PARAGRAPH.CENTER
 
-document.add_heading('Invoice Generation')
+print "Invoicerator 1.0\nGenerate invoices as Word Documents."
+print "Who is this invoice for?"
+invClient = raw_input("Company Name >")
+clientP = document.add_paragraph('Invoicing to: ')
+clientP.add_run(invClient)
+
 table = document.add_table(rows=1, cols=3)
 
 headerCells = table.rows[0].cells
@@ -12,6 +23,8 @@ headerCells[1].text = 'Project'
 headerCells[2].text = 'Hours'
 
 totalRows = 0
+hoursWorked = 0
+
 while True:
 	table.add_row()
 	totalRows += 1
@@ -21,6 +34,8 @@ while True:
 	client = raw_input(">")
 	print "Hours worked:"
 	hours = raw_input(">")
+	hoursWorked += int(hours)
+
 	print "Date: %s, Client: %s, Hours: %s" %(date, client, hours)
 	entry = table.rows[totalRows]
 	entry.cells[0].text = date
@@ -31,7 +46,22 @@ while True:
 	if answer == 'y':
 		continue
 	else:
-		print "Ok, saving."
+		print "Ok, finished making your table."
 		break
+rate = input("What is your rate? >")
+total = hoursWorked * rate
+print "You are owed $%d." %total
 
-document.save('invoiceGen.docs')
+rateP = document.add_paragraph()
+rateP.add_run("Rate: ").bold=True
+rateP.add_run("$%d" %rate)
+tHours = document.add_paragraph()
+tHours.add_run("Total Hours: ").bold=True
+tHours.add_run('%s' %hoursWorked)
+payment = document.add_paragraph()
+payment.add_run("Total Owed: ").bold=True
+payment.add_run("$%d" %total)
+document.add_paragraph()
+
+document.add_paragraph('Total is due within 30 days of receiving this invoice.')
+document.save('GeneratedInvoice.docx')
